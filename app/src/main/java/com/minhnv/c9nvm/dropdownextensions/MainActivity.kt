c08db4862d7adaff624a0eeec2bc58aab2ede340
Nguyen Van Minh <minhnv@miichisoft.net>
2021-04-23 14:44:44 +0700

    test ci

--- app/src/main/java/com/minhnv/c9nvm/dropdownextensions/MainActivity.kt
+++ app/src/main/java/com/minhnv/c9nvm/dropdownextensions/MainActivity.kt
@@ -9,13 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val dpDemo: DropDown = findViewById(R.id.dpDemo)
         val list = mutableListOf(
-            DropDownObj("1"),
-            DropDownObj("2"),
-            DropDownObj("3"),
-            DropDownObj("4"),
-            DropDownObj("5")
+            DropDownObj("1"), DropDownObj("2"), DropDownObj("3"), DropDownObj("4"), DropDownObj("5")
         )
         //not needed
         dpDemo.setListData(list)
@@ -23,7 +21,13 @@ class MainActivity : AppCompatActivity() {
             override fun onSelected(text: String, position: Int) {
                 Toast.makeText(this@MainActivity, "value selected $text", Toast.LENGTH_SHORT).show()
             }
+
         })
 
+
     }
+
+
+
+
 }
